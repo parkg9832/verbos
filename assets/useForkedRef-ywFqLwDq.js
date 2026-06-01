535a5c5c36c0d6ebfe58c60c43a312af6068ed01
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-01 10:35:36 +0900

    Updates

---
+++ assets/useForkedRef-ywFqLwDq.js
@@ -0,0 +1,1 @@
+import{C as e,D as t}from"./index-D74jasd-.js";var n=t(e());function r(...e){return(0,n.useMemo)(()=>e.every(e=>e==null)?null:t=>{e.forEach(e=>{i(e,t)})},e)}function i(e,t){if(e!=null)if(a(e))e(t);else try{e.current=t}catch{throw Error(`Cannot assign value "${t}" to ref "${e}"`)}}function a(e){return!!(e&&{}.toString.call(e)==`[object Function]`)}export{r as t};
