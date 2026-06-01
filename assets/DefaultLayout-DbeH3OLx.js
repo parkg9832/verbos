43c5b4352c5b3633746a711dcb423daea24ac83e
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-01 13:21:39 +0900

    Updates

---
+++ assets/DefaultLayout-DbeH3OLx.js
@@ -0,0 +1,9 @@
+import{T as e,a as t,o as n,w as r}from"./components-C2g2QlpO.js";import{C as i,t as a}from"./index-DuX1B9_g.js";i();var o=a(),s=()=>(0,o.jsxs)(`div`,{children:[(0,o.jsx)(`style`,{children:`
+        /* Expand containers to full width without breaking sidebar spacing */
+        .container-fluid,
+        .container-lg,
+        .container {
+          max-width: 100% !important;
+          width: 100% !important;
+        }
+      `}),(0,o.jsx)(t,{}),(0,o.jsxs)(`div`,{className:`wrapper d-flex flex-column min-vh-100`,children:[(0,o.jsx)(n,{}),(0,o.jsx)(`div`,{className:`body flex-grow-1`,children:(0,o.jsx)(e,{})}),(0,o.jsx)(r,{})]})]});export{s as default};
