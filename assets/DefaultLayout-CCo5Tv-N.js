832523ed052942396908f755e8c4146d3e27f5d1
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-01 13:13:16 +0900

    Updates

---
+++ assets/DefaultLayout-CCo5Tv-N.js
@@ -0,0 +1,9 @@
+import{T as e,a as t,o as n,w as r}from"./components-CSwUDTlf.js";import{C as i,t as a}from"./index-BGgQyrXE.js";i();var o=a(),s=()=>(0,o.jsxs)(`div`,{children:[(0,o.jsx)(`style`,{children:`
+        /* Expand containers to full width without breaking sidebar spacing */
+        .container-fluid,
+        .container-lg,
+        .container {
+          max-width: 100% !important;
+          width: 100% !important;
+        }
+      `}),(0,o.jsx)(t,{}),(0,o.jsxs)(`div`,{className:`wrapper d-flex flex-column min-vh-100`,children:[(0,o.jsx)(n,{}),(0,o.jsx)(`div`,{className:`body flex-grow-1`,children:(0,o.jsx)(e,{})}),(0,o.jsx)(r,{})]})]});export{s as default};
