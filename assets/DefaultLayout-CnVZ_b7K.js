2e15154dac65e17036a89fdb85f6a05b2d71bf2c
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-01 12:50:25 +0900

    Updates

---
+++ assets/DefaultLayout-CnVZ_b7K.js
@@ -0,0 +1,22 @@
+import{C as e,S as t,a as n,o as r}from"./components-C4K-iyeZ.js";import{C as i,t as a}from"./index-CdLn0Ojn.js";i();var o=a(),s=()=>(0,o.jsxs)(`div`,{children:[(0,o.jsx)(`style`,{children:`
+        /* Force container stretching to eliminate invisible boxes on desktop */
+        .body,
+        .wrapper,
+        .container-fluid,
+        .container-lg,
+        .container {
+          max-width: 100% !important;
+          width: 100% !important;
+          margin-left: 0 !important;
+          margin-right: 0 !important;
+          padding-left: 0 !important;
+          padding-right: 0 !important;
+        }
+        .px-4 {
+          padding-left: 1.5rem !important;
+          padding-right: 1.5rem !important;
+        }
+        .body {
+          padding: 1.5rem !important;
+        }
+      `}),(0,o.jsx)(n,{}),(0,o.jsxs)(`div`,{className:`wrapper d-flex flex-column min-vh-100`,children:[(0,o.jsx)(r,{}),(0,o.jsx)(`div`,{className:`body flex-grow-1`,children:(0,o.jsx)(e,{})}),(0,o.jsx)(t,{})]})]});export{s as default};
