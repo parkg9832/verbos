2ee6b12af5b36e038b616abaf5872f7d80d85853
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-03 15:34:26 +0900

    Updates

--- assets/CCardBody-DoZoFQ5D.js
+++
@@ -1,1 +0,0 @@
-import{C as e,D as t,d as n,f as r,u as i}from"./index-COceIueh.js";var a=t(e()),o=t(n()),s=(0,a.forwardRef)((e,t)=>{var{children:n,className:o}=e,s=r(e,[`children`,`className`]);return a.createElement(`div`,Object.assign({className:i(`card-body`,o)},s,{ref:t}),n)});s.propTypes={children:o.default.node,className:o.default.string},s.displayName=`CCardBody`;export{s as t};
